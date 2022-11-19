5a2648b9430234b1cb337cf65bdc60c5a2868c69
Dan Haywood <dan@haywood-associates.co.uk>
2022-11-19 13:34:27 +0000

    adds in sqlextended properties

--- MovieDb/dbo/Tables/Movie.sql
+++ MovieDb/dbo/Tables/Movie.sql
@@ -7,3 +7,21 @@
     CONSTRAINT [PK_Movie] PRIMARY KEY CLUSTERED ([ID] ASC)
 );
 
+
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'gdpr_category', @value = N'sensitive', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'Movie', @level2type = N'COLUMN', @level2name = N'Title';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'gdpr_brand', @value = N'whatever', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'Movie', @level2type = N'COLUMN', @level2name = N'Title';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'gdpr_category', @value = N'not_sensitive', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'Movie', @level2type = N'COLUMN', @level2name = N'ReleaseDate';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'gdpr_brand', @value = N'whatever_2', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'Movie', @level2type = N'COLUMN', @level2name = N'ReleaseDate';
+
