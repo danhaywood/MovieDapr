00e450f641f6e5e3beb0e5f9e79636bf39df4ce4
Dan Haywood <dan@haywood-associates.co.uk>
2022-11-19 13:49:49 +0000

    adds evolve nuget, adds some scripts

---
+++ RazorPagesMovie/Sql/V20221119_1348__Movie_extended_properties2.sql
@@ -0,0 +1,6 @@
+﻿EXECUTE sp_addextendedproperty @name = N'gdpr_category', @value = N'not_sensitive', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'Movie', @level2type = N'COLUMN', @level2name = N'ReleaseDate';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'gdpr_brand', @value = N'whatever_2', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'Movie', @level2type = N'COLUMN', @level2name = N'ReleaseDate';
+
